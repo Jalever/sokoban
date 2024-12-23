48d181553ba020f78f8019e8cc08b98aeca4d54d
Friedrich Chen <friedrichchen@Inuyasha.local>
2024-12-23 10:34:03 +0800

    feat: setup a window with nothing else

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::{conf, event, Context, GameResult};
+
+struct Game {}
+
+impl event::EventHandler<ggez::GameError> for Game {
+    fn update(&mut self, _context: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, _context: &mut Context) -> GameResult {
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let context_builder = ggez::ContextBuilder::new("sokoban", "jalever")
+        .window_setup(conf::WindowSetup::default().title("Sokoban"))
+        .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0))
+        .add_resource_path("./resources");
+
+    let (context, event_loop) = context_builder.build()?;
+    let game = Game {};
+    event::run(context, event_loop, game)
 }
