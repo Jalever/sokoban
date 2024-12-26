2d9426ef6bb2089780bb2b99321cfd84d4c1b983
Friedrich Chen <friedrichchen@Inuyasha.local>
2024-12-26 09:35:00 +0800

    feat: create entities and components

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use ggez::{conf, event, Context, GameResult};
+use hecs::{Entity, World};
+use std::path;
 
-struct Game {}
+struct Game {
+    world: World,
+}
 
 impl event::EventHandler<ggez::GameError> for Game {
     fn update(&mut self, _context: &mut Context) -> GameResult {
@@ -12,13 +16,80 @@ impl event::EventHandler<ggez::GameError> for Game {
     }
 }
 
+#[allow(dead_code)]
+pub struct Position {
+    x: u8,
+    y: u8,
+    z: u8,
+}
+
+#[allow(dead_code)]
+pub struct Renderable {
+    path: String,
+}
+
+pub struct Player {}
+
+pub struct Wall {}
+pub struct Box {}
+pub struct BoxSpot {}
+
+pub fn create_wall(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/wall.png".to_string(),
+        },
+        Wall {},
+    ))
+}
+
+pub fn create_floor(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 5, ..position },
+        Renderable {
+            path: "/images/floor.png".to_string(),
+        },
+    ))
+}
+
+pub fn create_box(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/box.png".to_string(),
+        },
+        Box {},
+    ))
+}
+
+pub fn create_box_player(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 9, ..position },
+        Renderable {
+            path: "/images/box_spot.png".to_string(),
+        },
+    ))
+}
+
+pub fn create_player(world: &mut World, position: Position) -> Entity {
+    world.spawn((
+        Position { z: 10, ..position },
+        Renderable {
+            path: "/images/player.png".to_string(),
+        },
+    ))
+}
+
 pub fn main() -> GameResult {
+    let world = World::new();
+
     let context_builder = ggez::ContextBuilder::new("sokoban", "jalever")
         .window_setup(conf::WindowSetup::default().title("Sokoban"))
         .window_mode(conf::WindowMode::default().dimensions(800.0, 600.0))
-        .add_resource_path("./resources");
+        .add_resource_path(path::PathBuf::from("./resources"));
 
     let (context, event_loop) = context_builder.build()?;
-    let game = Game {};
+    let game = Game { world };
     event::run(context, event_loop, game)
 }
